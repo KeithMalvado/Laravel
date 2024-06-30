f03794a393db89238cf553832acd32ce8fd35e31
ipantex <akhmadfauzisetyawan@gmail.com>
2024-07-01 00:58:09 +0700

    first commit

---
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Siswa', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('Nama Siswa'100);
+            $table->string('Alamat'100);
+            $table->string('tgllhr',);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Siswa');
+    }
+}
